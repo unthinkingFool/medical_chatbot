21eba19642bc7abd5c843674e6e6df0347e140a2
unthinkingFool <swapnilpankajbeauty@gmail.com>
2026-03-25 19:18:03 +0600

    requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Swapnil Das",
+    author_email="swapnilpankajbeauty@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
